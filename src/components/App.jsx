9cf2c96c08c955ca070d11966c49bcce7487e4e1
Tobias Viana <tobias.viana@vmlbrasil.com>
2017-05-18 11:04:55 -0300

    Base state with scss and react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import OutroComponente from './OutroComponente';
-import Test from './Test';
+// Libs
+import React from "react";
 
+// Components
+import SimpleStatelessComponent from './SimpleStatelessComponent';
+
+//CSS
 require("../scss/test");
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div className="hello-world" style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-        <OutroComponente />
+        <h1>Hello World, im a app container</h1>
+
+        <SimpleStatelessComponent />
       </div>);
 	}
 }
